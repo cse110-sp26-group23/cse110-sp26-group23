025b09538dbd755cf0964a591aa4ea17ffe5ad8f
BrendanBarber <brendan.t.barber@gmail.com>
2026-05-19 11:48:43 -0700

    Update app.js, game.js, gameEngine.js, and renderPane.js to have same file header convention

--- source/js/game.js
+++ source/js/game.js
@@ -1,3 +1,11 @@
+/**
+ * @file Game-screen bootstrap.
+ *
+ * Entry point for game.html. On DOMContentLoaded, initializes the render
+ * pane and starts the game engine, then exposes a small handle on
+ * window.__game for in-browser debugging.
+ */
+
 import { initRenderPane } from './renderPane.js';
 import { startGame, getGameState } from './gameEngine.js';
 
